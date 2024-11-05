9a05c282180f8ddeab571e8849be3beb62ac1329
Samuel-SouzaZz <samucael8832@gmail.com>
2024-11-05 20:01:40 -0300

    implementando código no arquivo userBusiness.ts

--- src/business/userBusiness.ts
+++ src/business/userBusiness.ts
@@ -1,5 +1,28 @@
-import {generateId} from "../services/idGenerator";
+import { generateId } from "../services/idGenerator";
+import {cadrastroUsuario} from "../data/userData";
 
-export class UserBusiness{
-    cadastro = async(username:String, email:String, senha:String, telefone:Number, cpf:String) =>{}
+export class UserBusiness {
+    cadastro = async (username: string, email: string, senha: string, telefone: number, cpf: string) => {
+        try {
+            if (!username || !email || !senha) {
+                throw new Error("Campos vazios");
+            }
+
+            if (email.indexOf("@") === -1 || email.indexOf("@") === 0) {
+                throw new Error("Email inválido");
+            }
+
+            if (senha.length < 6) {
+                throw new Error("Senha inválida");
+            }
+
+            const id = generateId();
+            const user = await cadrastroUsuario(id,username,email,senha,telefone,cpf);
+            
+            return { id, username, email, telefone, cpf };
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,25 @@
 import {app} from "./App";
 import { userRouter } from "./routers/userRouter";
 
-app.post('/users/cadastro', userRouter)
+app.post('/users/cadastro', userRouter);
+
+// 1
+//app.post('users/login', userRouter)
+// 2
+//app.post('/users/atualizarDadosEsp', userRouter)
+// 3
+//app.post('/users/atualizarDados', userRouter)
+//4
+//app.post('/users/deletarUser', userRouter)
+//5
+//app.post('/books/buscarLivros', booksRouter)
+//6
+//app.post('/books/bucarTodosLivros', booksRouter)
+//7
+//app.post('/rent/deletarEmprestimo', rentRouter)
+//8
+//app.post('/rent/buscarLivros', rentRouter)
+//9
+//app.post('/rent/addLivros', rentRouter)
+//10
+//app.post('/rent/buscarLivrosCliente', rentRouter)
